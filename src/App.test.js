50fb6c935bea9a541e0158dc81d5fa6b4b9c1a57
Amber-Pittman <amber@amberpittman.com>
2020-03-12 19:09:14 -0400

    Created some unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { add, subtract }from "./App";
+import { add, subtract } from "./App";
 
 // test("Fake test", () => {
 //     // assertion
@@ -7,14 +7,23 @@ import { add, subtract }from "./App";
 
 console.log(add(1, 2));
 
+
+// Unit Test - it only tests one thing
+// The add function is a unit
 test("add", () => {
-    const value = add(1,2);
-    // assertion
-    expect(value).toBe(3);
+    // const value = add(1,2);
+    // // assertion
+    // expect(value).toBe(3);
+    expect(add(1, 2)).toBe(3);
+    expect(add(5, 2)).toBe(7);
 });
 
+
+// This is another unit test
 test("subtract", () => {
     const value = subtract(5,3);
     // assertion
     expect(value).toBe(2);
+    expect(subtract(8, 4)).toBe(4);
+    expect(subtract(15, 9)).toBe(6);
 });
